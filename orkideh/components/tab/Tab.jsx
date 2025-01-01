dec5752238fa4e1c4aa08bf51fd7ed605c720ec5
Anahitashams <anahitashams01@gmail.com>
2025-01-01 04:11:33 -0800

    end tab

--- orkideh/components/tab/Tab.jsx
+++ orkideh/components/tab/Tab.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { GoPerson } from "react-icons/go";
+import { PiShoppingCartLight } from "react-icons/pi";
+
+
 
 function Tab() {
   return (
     <>
-  
+      <div className='w-full h-[60px] flex flex-row-reverse'>
+        <div className="flex justify-end w-[50%]"><img className="w-[80px] h-[40px] mr-[20px] mt-[15px]" src="public/img/logo.svg" alt="" /></div>
+        <div className="flex justify-start w-[50%]">
+          <div className="ml-[20px] m-2 mt-[20px] text-2xl"> <GoPerson /></div>
+          <div className="m-2  mt-[20px] text-2xl"><PiShoppingCartLight /></div>
+        </div>
+      </div>
     </>
   )
 }
